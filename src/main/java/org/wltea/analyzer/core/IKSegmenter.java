d8a07528e1d9d1911282899f611814c1171a4726
Davis Liu <davischahliu@gmail.com>
2016-03-08 16:52:38 +0800

    remove no-chinese segments

--- src/main/java/org/wltea/analyzer/core/IKSegmenter.java
+++ src/main/java/org/wltea/analyzer/core/IKSegmenter.java
@@ -98,9 +98,9 @@ public final class IKSegmenter {
 	private List<ISegmenter> loadSegmenters(){
 		List<ISegmenter> segmenters = new ArrayList<ISegmenter>(4);
 		//处理字母的子分词器
-		segmenters.add(new LetterSegmenter()); 
+		//segmenters.add(new LetterSegmenter()); 
 		//处理中文数量词的子分词器
-		segmenters.add(new CN_QuantifierSegmenter());
+		//segmenters.add(new CN_QuantifierSegmenter());
 		//处理中文词的子分词器
 		segmenters.add(new CJKSegmenter());
 		return segmenters;
